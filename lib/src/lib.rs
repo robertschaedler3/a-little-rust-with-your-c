e506065488afb7ff095284f7f8be7c1bbefe4db1
Robert Schaedler <robertschaedler3@gmail.com>
2021-11-17 12:02:34 -0500

    create Rust lib

---
+++ lib/src/lib.rs
@@ -0,0 +1,15 @@
+use libc::{c_char};
+use std::ffi::CStr;
+
+#[no_mangle]
+pub extern "C" fn say_hello(name: *const c_char) {
+    let name = unsafe {
+        if name.is_null() {
+            "World".to_string()
+        } else {
+            CStr::from_ptr(name).to_str().unwrap().to_string()
+        }
+    };  
+
+    println!("Hello, {}!", name);
+}
