9890e68f4c36a88bc8ae9f7d28715862ddd83a00
Robert Schaedler <rschaedler@microsoft.com>
2021-11-17 17:49:35 +0000

    update unsafe code

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,15 +1,15 @@
-use libc::{c_char};
+use libc::c_char;
 use std::ffi::CStr;
 
 #[no_mangle]
 pub extern "C" fn say_hello(name: *const c_char) {
-    let name = unsafe {
+    let name = {
         if name.is_null() {
             "World".to_string()
         } else {
-            CStr::from_ptr(name).to_str().unwrap().to_string()
+            unsafe { CStr::from_ptr(name).to_str().unwrap().to_string() }
         }
-    };  
+    };
 
     println!("Hello, {}!", name);
 }
