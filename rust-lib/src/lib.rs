1889cbf41f12427a91956aede34c86164f91865c
Robert Schaedler <rschaedler@microsoft.com>
2024-06-08 06:10:52 +0000

    update lib

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -3,7 +3,7 @@ use std::ffi::CStr;
 
 #[no_mangle]
 pub extern "C" fn say_hello(input: *const c_char) {
-    let mut name = "world";
+    let mut name = "C/C++";
 
     if !input.is_null() {
         unsafe {
